df92012cabacd3607a9117d572ab78e13ad9edd3
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2022-11-11 19:11:54 +0530

    [ADDED]Bhavesh:C Language Graphics Program to draw Random Circles

---
+++ RANDOMCI.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include"stdlib.h"
+void main()
+{
+	int gd=DETECT,gm,i=0,x,xx,y,yy;
+	initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
+	x=getmaxx();
+	y=getmaxy();
+	while(!kbhit())
+	{
+	i++;
+	circle(xx=random(x),yy=random(y),random(30));
+	setfillstyle(random(i),random(30));
+	floodfill(xx,yy,getmaxcolor());
+	}
+	getch();
+}
